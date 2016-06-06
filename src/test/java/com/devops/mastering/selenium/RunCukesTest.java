b00232ccd1cf726ad3bdcbe95c90b911b444013a
Michael L. Lively <mlively@gaig.com>
2016-06-06 13:48:45 -0400

    project 3

---
+++ src/test/java/com/devops/mastering/selenium/RunCukesTest.java
@@ -0,0 +1,12 @@
+package com.devops.mastering.selenium;
+
+import org.junit.runner.RunWith;
+import cucumber.junit.Cucumber;
+
+
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(format = {"pretty", "html:target/cucumber-htmlreport","json-pretty:target/cucumber-report.json"})
+
+public class RunCukesTest {
+}
